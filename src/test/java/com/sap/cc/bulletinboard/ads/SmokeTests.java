2a07da27eb50d32bbb60837526653fe7bea425ba
avikmullick <avikmullick10@gmail.com>
2023-09-05 22:38:25 +0530

    Enable Smoke Test

--- src/test/java/com/sap/cc/bulletinboard/ads/SmokeTests.java
+++ src/test/java/com/sap/cc/bulletinboard/ads/SmokeTests.java
@@ -16,7 +16,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Disabled
 public class SmokeTests {
     
 //██╗  ██╗ █████╗ ███╗   ██╗██████╗ ███████╗     ██████╗ ███████╗███████╗██╗██╗██╗
